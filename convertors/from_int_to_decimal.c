e14dfb53c578ecf82d132877c23f2dbbcbd88faf
hester999 <hester@MacBook-Air-Ivan.local>
2024-01-17 02:39:15 +0300

    add int to decimal and test 'int_to_decimal.c'

--- convertors/from_int_to_decimal.c
+++ convertors/from_int_to_decimal.c
@@ -6,4 +6,39 @@
  * @return 0 - OK
  *         1 - ошибка конвертации
  */
-int s21_from_int_to_decimal(int src, s21_decimal *dst) {}
+int s21_from_int_to_decimal(int src, s21_decimal *dst) {
+    int temp_arr[32] = {0}; // массив для хранения 2 представления int
+    int ost;
+    int i = 0;
+    int flag = 0;
+
+    if(src <0){
+
+        dst->bits[3] = dst->bits[3] << 31;
+        dst->bits[3]|=1;
+        src*=-1;
+    }
+
+    int temp = src;
+
+    while(temp > 0){ // конвертируем 10 в 2
+        ost = temp % 2;
+        temp_arr[i] = ost;
+        i++;
+        temp = temp / 2;
+    }
+
+    for(int j = 0; j<i; j++){
+
+        temp_arr[j] = temp_arr[j]<<j;
+        dst->bits[0] |= temp_arr[j];
+    }
+
+
+    if(dst->bits[0] != src){
+        flag=1;
+    }
+
+
+    return flag;
+}
