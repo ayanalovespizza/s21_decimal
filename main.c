48096010c0a5344963e2cb5e7f47a7df39ee4633
hester999 <ivankuzin09@gmail.com>
2024-01-17 23:32:19 +0300

    fix overflow

--- main.c
+++ main.c
@@ -1,32 +1,17 @@
 #include <stdio.h>
 
 #include "s21_decimal.h"
+#include <limits.h>
 
 int main() {
-  //   s21_decimal decimal1 = {0x00000000, 0x00000000, 0x00000000, 0x00000000};
-  //   s21_decimal decimal2 = {0x00000000, 0x00000000, 0x00000000, 0x00000000};
 
-  //   printf("Результат теста 1 (должно быть 0): %d\n\n",
-  //          s21_is_equal(decimal1, decimal2));
+  s21_decimal decimal ={0};
+  int a = INT_MIN;
+  int res = s21_from_int_to_decimal(a,&decimal);
+  
+  // printf("%d\n",decimal.bits[0]);
+  // printf("%x\n",decimal.bits[3]);
+  printf("%d\n",res);
 
-  //   printf(
-  //       "Бит[0]: %d\n\
-// Бит[1]: %d\n\
-// Бит[2]: %d\n\
-// Бит[3] aka степень: %d\n\n",
-  //       decimal1.bits[0], decimal1.bits[1], decimal1.bits[2],
-  //       decimal1.bits[3]);
-
-  //   printf(
-  //       "Бит[0]: %d\n\
-// Бит[1]: %d\n\
-// Бит[2]: %d\n\
-// Бит[3] aka степень: %d\n",
-  //       decimal2.bits[0], decimal2.bits[1], decimal2.bits[2],
-  //       decimal2.bits[3]);
-
-  int x = MINUS;
-
-  printf("%d\n", x);
   return 0;
 }
